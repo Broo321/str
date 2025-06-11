d418b28036a51d08453d2632cd70a4e4326f70bb
Witaj_Bro <122749501+Broo321@users.noreply.github.com>
2025-06-11 20:45:18 +0200

    Update ok.js

--- assets/JS/ok.js
+++ assets/JS/ok.js
@@ -1,23 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
+  const nav = document.querySelector(".nawigacja");
+  const hamburger = document.querySelector(".hamburger");
+  const headerHeight = 245;
+
+  document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", e => {
       e.preventDefault();
-      const target = document.querySelector(this.getAttribute("href"));
-      if (target) {
-        target.scrollIntoView({ behavior: "smooth" });
-      }
-      const nav = document.querySelector(".nawigacja");
-      const hamburger = document.querySelector(".hamburger");
-      if (nav.classList.contains("active")) {
-        nav.classList.remove("active");
-        hamburger.classList.remove("active");
-      }
+      
+      const id = link.getAttribute("href");
+      if (id === "#") return;
+      const target = document.querySelector(id);
+      if (!target) return;
+      const targetY = target.offsetTop - headerHeight;
+
+      window.scrollTo({
+        top: targetY,
+        behavior: "smooth"
+      });
+
+      nav?.classList.remove("active");
+      hamburger?.classList.remove("active");
     });
   });
-  const hamburger = document.querySelector(".hamburger");
-  const nav = document.querySelector(".nawigacja");
 
-  hamburger.addEventListener("click", () => {
+  hamburger?.addEventListener("click", () => {
     nav.classList.toggle("active");
     hamburger.classList.toggle("active");
   });
